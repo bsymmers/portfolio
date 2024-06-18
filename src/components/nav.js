0f043366bd5048b7d3fed79ce8a540f176d40b6f
Brandon Symmers <brandon.symmers@gmail.com>
2024-06-17 18:59:26 -0700

    added fonts + titling

---
+++ src/components/nav.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { Heading } from '@chakra-ui/react'
+function Nav() {
+  return (
+    <div>
+      <Heading>Brandon Symmers</Heading>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+import '@fontsource/dm-serif-display'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'DM Serif Display', dm-serif-display`,
+    body: `'DM Serif Display', dm-serif-display`
+  }
+})
+
+export default theme
